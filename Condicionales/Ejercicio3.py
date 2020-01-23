b4a6526d62dbec51e7e1d50e4304571d96291242
fran <fran8454@hotmail.com>
2020-01-23 18:45:59 +0100

    subiendo ejercicio3

---
+++ Condicionales/Ejercicio3.py
@@ -0,0 +1,8 @@
+
+letra = input("Inserte un caracter :").lower()
+
+
+if letra=='a' or letra=='e' or letra=='i' or letra=='o' or letra=='u':
+    print("es una vocal")
+else:
+    print("no es una vocal")    
